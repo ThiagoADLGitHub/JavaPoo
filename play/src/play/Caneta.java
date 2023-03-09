97cf234092b44732401c5f8584e4d0480c20acca
ThiagoADLGitHub <126714187+ThiagoADLGitHub@users.noreply.github.com>
2023-03-09 16:39:03 -0300

    declarações dos atributos

---
+++ play/src/play/Caneta.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package play;
+
+/**
+ *
+ * @author thiag
+ */
+public class Caneta {
+    
+    public String modelo;
+    public String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    
+}
